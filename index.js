713b3a6a37ecefabace7f06307d8a4cef198f7a5
Tiago Fernandes <tiago.soares@cmrad.com>
2024-06-28 20:51:54 +0200

    add rss, back links

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const init = async (sourceDir) => {
   const directory = fs.readdirSync(sourceDir)
   await directory.forEach(async (filenameExt) => {
     if (filenameExt.endsWith(".md") && filenameExt !== "index.md") {
-      console.log("---", filenameExt);
+      // console.log("---", filenameExt);
       const [title, data, file, createdDate, modifiedDate] = await getMdMetadata(filenameExt);
       blogItems.push({ title, file, filenameExt, createdDate, modifiedDate, data });
     }
@@ -17,7 +17,7 @@ const init = async (sourceDir) => {
     return (new Date(b.createdDate) - new Date(a.createdDate));
   }).reverse();
 
-  console.log("blogItems", sortedBlogItems);
+  // console.log("blogItems", sortedBlogItems);
 
   const blogIndex = createIndexMd(sortedBlogItems);
   const sitemapBlog = createSitemapBlog(sortedBlogItems);
@@ -26,6 +26,7 @@ const init = async (sourceDir) => {
   // 
 
   writeBlogItemsToHtml(sortedBlogItems);
+  createRss(sortedBlogItems);
 };
 
 const getMdMetadata = async (item) => {
@@ -34,7 +35,7 @@ const getMdMetadata = async (item) => {
 
   // Access mtime (last modified time)
   const formatter = new Intl.DateTimeFormat('en-GB', {
-    dateStyle: 'full',
+    dateStyle: 'short',
     timeStyle: 'long',
     timeZone: 'Europe/Madrid',
   })
@@ -43,27 +44,29 @@ const getMdMetadata = async (item) => {
   // const modifiedDate = (stats.mtime);
   // const createdDate = (stats.birthtime);
 
-  console.log(`File was last modified on: ${modifiedDate}`);
-  console.log(`File was created on: ${createdDate}`);
-  console.log("stats", stats);
+  // console.log(`File was last modified on: ${modifiedDate}`);
+  // console.log(`File was created on: ${createdDate}`);
+  // console.log("stats", stats);
   let data = fs.readFileSync(`./blog-builder/${item}`, "utf8");
-  data = data.concat(`\n\n--- created at: ${createdDate} \n\n`);
-  data = data.concat(`\n\n--- modifiedDate at: ${modifiedDate} \n\n`);
-  console.log("data", data);
+  data = data.concat(`\n\n originally published: ${createdDate}\n`);
+  data = data.concat(`\n last updated: ${modifiedDate} \n\n`);
+  // console.log("data", data);
 
   const title = data.match(/^# (.*)$/m)[1];
-  console.log("title", title);
+  // console.log("title", title);
   const file = item.replace(".md", "");
   // console.log("makre--------", markdownify(data))
   return [title, data, file, createdDate, modifiedDate];
 }
 
 const createIndexMd = (blogItems) => {
-  const indexData = "# Blog Index\n\n"
+  const indexData = ""
   const indexDataItems = blogItems.map((item) => {
-    return `* [${item.title}](./${item.filenameExt}) - created at: ${item.createdDate} - modified at: ${item.modifiedDate}`;
+    return `## [${item.title}](./${item.filenameExt})
+###### published: ${item.createdDate}
+`;
   }).join("\n");
-  console.log("indexData", indexData);
+  // console.log("indexData", indexData);
   const template = fs.readFileSync('./blog-builder/templates/template.html', "utf8");
 
   writeMdToHtmlFile(indexData.concat(indexDataItems), 'index', template)
@@ -74,22 +77,46 @@ const createSitemapBlog = (blogItems) => {
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">`
   const sitemapFooter = `</urlset>`
 
-  const sitemapData = blogItems.map((item) => {
-    console.log("item", item);
+  const rssItems = blogItems.map((item) => {
+    // console.log("item", item);
     return `<url><loc>https://www.tiago.tf/blog/${item.file}.html</loc><lastmod>${item.modifiedDate}</lastmod></url>`;
   }).join("\n");
 
-  fs.writeFileSync("./blog/sitemap.xml", sitemapDataHeader.concat(sitemapData).concat(sitemapFooter));
+  fs.writeFileSync("./blog/sitemap.xml", sitemapDataHeader.concat(rssItems).concat(sitemapFooter));
+}
+
+const createRss = (blogItems) => {
+  const sitemapDataHeader = `<?xml version="1.0" encoding="UTF-8" ?><rss version="2.0"><channel>`
+  const sitemapFooter = `</channel></rss>`
+
+  const headerData = `<title>Tiago Fernandes - thoughts</title>
+        <link>http://www.tiago.tf/blog</link>
+        <description>Tiago Fernandes - thoughts</description>
+        <language>en-gb</language>
+        <lastBuildDate>${new Date()}</lastBuildDate>`
+
+
+  const sitemapData = blogItems.map((item) => {
+    // console.log("item", item);
+    return `<item>
+            <title>${item.title}</title>
+            <link>http://www.tiago.tf/blog/${item.file}</link>
+            <description>This is the description for the first article.</description>
+            <author>Tiago Fernandes</author>
+            <pubDate>${item.createdDate}</pubDate>
+            <guid>http://www.tiago.tf/blog/${item.file}</guid>
+        </item>`;
+  }).join("\n");
+
+  fs.writeFileSync("./blog/rss.xml", sitemapDataHeader.concat(headerData).concat(sitemapData).concat(sitemapFooter));
 }
 
 const writeBlogItemsToHtml = (blogItems) => {
-  console.log("writeBlogItemsToHtml", blogItems.length)
+  // console.log("writeBlogItemsToHtml", blogItems.length)
   const template = fs.readFileSync('./blog-builder/templates/template.html', "utf8");
   return blogItems.forEach((blogItem) => {
     writeMdToHtmlFile(blogItem.data, blogItem.file, template)
   });
-
-
 }
 
 
@@ -98,7 +125,6 @@ const writeMdToHtmlFile = (mdData, filename, template) => {
   output = output.replace(/href="(.*)\.md"/g, 'href="$1.html"');
   fs.writeFileSync(`./blog/${filename}.html`, output);
 }
-
 
 function markdownify(content) {
   marked.use({
@@ -109,9 +135,4 @@ function markdownify(content) {
   return marked.parse(content);
 }
 
-// const dir = process.argv.slice(2);
-// console.log("dir", dir)
-// if (dir.length < 1) {
-//   throw new Error("needs to define a path to organise")
-// }
 init('./blog-builder');
